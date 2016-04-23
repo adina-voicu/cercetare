8831883eab7de1ffcc580238acf1eb87c4bf2988
adina-voicu <s.adynna@yahoo.com>
2016-04-23 23:28:32 +0300

    Added files via upload

---
+++ LSI-test.py
@@ -0,0 +1,77 @@
+# your code goes here
+
+from numpy import zeros
+from scipy.linalg import svd
+#following needed for TFIDF
+from math import log
+from numpy import asarray, sum
+
+titles = ["A Rule-based Language for Deductive Object-Oriented Databases."
+          "Bill-of-Material Configuration Generation."
+          "Title, General Chairman's Message, Program Chairman's Message, Reviewers, Table of Contents, Author Index."
+          "The Generalized Grid File: Description and Performance Aspects."
+          "Join Index, Materialized View, and Hybrid-Hash Join: A Performance Analysis."
+          "Compilation of Logic Programs to Implement Very Large Knowledge Base Systems - A Case Study: Educe*."
+          "Attribute Inheritance Implemented on Top of a Relational Database System."
+          "Update Propagation in Distributed Memory Hierarchy."
+          "Modeling Design Object Relationships in PEGASUS."
+          "Experimental Evaluation of Concurrency Checkpointing and Rollback-Recovery Algorithms.
+          "An Attribute-Oriented Approach for Learning Classification Rules from Relational Databases."
+          "Generalization and a Framework for Query Modification."
+          "Selectivity Estimation Using Homogeneity Measurement." ]
+stopwords = ['a','and','edition','for','in','little','of','the','to']
+ignorechars = ''',:'!'''
+
+class LSA(object):
+    def __init__(self, stopwords, ignorechars):
+        self.stopwords = stopwords
+        self.ignorechars = ignorechars
+        self.wdict = {}
+        self.dcount = 0        
+    def parse(self, doc):
+        words = doc.split();
+        for w in words:
+            w = w.lower().translate(None, self.ignorechars)
+            if w in self.stopwords:
+                continue
+            elif w in self.wdict:
+                self.wdict[w].append(self.dcount)
+            else:
+                self.wdict[w] = [self.dcount]
+        self.dcount += 1  
+        #print(self.wdict)
+    def build(self):
+        self.keys = [k for k in self.wdict.keys() if len(self.wdict[k]) > 1]
+        self.keys.sort()
+        self.A = zeros([len(self.keys), self.dcount])
+        for i, k in enumerate(self.keys):
+            for d in self.wdict[k]:
+                self.A[i,d] += 1
+    def calc(self):
+        self.U, self.S, self.Vt = svd(self.A)
+    def TFIDF(self):
+        WordsPerDoc = sum(self.A, axis=0)        
+        DocsPerWord = sum(asarray(self.A > 0, 'i'), axis=1)
+        rows, cols = self.A.shape
+        for i in range(rows):
+            for j in range(cols):
+                self.A[i,j] = (self.A[i,j] / WordsPerDoc[j]) * log(float(cols) / DocsPerWord[i])
+    def printA(self):
+        print ('Here is the count matrix')
+        print (self.A)
+        
+    def printSVD(self):
+        print ('Here are the singular values')
+        print (self.S)
+        print ('Here are the first 3 columns of the U matrix')
+        print (-1*self.U[:, 0:3])
+        print ('Here are the first 3 rows of the Vt matrix')
+        print (-1*self.Vt[0:3, :])
+        
+mylsa = LSA(stopwords, ignorechars)
+for t in titles:
+    mylsa.parse(t)
+mylsa.build()
+mylsa.printA()
+mylsa.calc()
+mylsa.printSVD()
