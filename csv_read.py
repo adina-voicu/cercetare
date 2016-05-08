4df745a26b370874b3e40947bc3362cc6202970a
adina-voicu <s.adynna@yahoo.com>
2016-05-08 13:58:53 +0300

    Update csv_read.py

--- csv_read.py
+++ csv_read.py
@@ -1,4 +1,37 @@
 import csv
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import pos_tag
+from nltk.stem import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
+wnl = WordNetLemmatizer()
+#obtain a clean sentence
+def clean(str):
+    str = ''.join([c for c in str if c in 'ABCDEFGHIJKLNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890 \''])
+    return str
+def clean_sentence(s):
+    for word in s:
+        lst_cleaned = []
+        for items in s:
+            lst_cleaned.append(clean(items))
+        return lst_cleaned
+         
+def lemma(txt):
+    a=[]
+    for i in txt:
+        x = ' '.join([wnl.lemmatize(i,j[0].lower()) if j[0].lower() in ['a','n','v'] else wnl.lemmatize(i) for i,j in pos_tag(word_tokenize(i))])
+        a.append(x)
+    return a
+
+def stop(txt):
+    x=[]
+    for w in txt.split():
+        if w not in stopwords:
+            x.append(w)
+        else:
+            continue
+    return ' '.join(x)
+
 with open('datec.csv') as csvfile:
     reader = csv.DictReader(csvfile)
     header= [ 'id', 'conf', 'title', 'author', 'year', 'abstract', 'eq', 'conf_short', 'theme']
@@ -6,7 +39,10 @@ with open('datec.csv') as csvfile:
     for line in reader:
         row={}
         for field in header:
-            row[field]=line[field]
+            if field == 'abstract':
+                row['abstract'] = stop(' '.join(lemma(clean_sentence(nltk.sent_tokenize(line['abstract'])))))
+            else:    
+                row[field]=line[field]
         output.append(row)
     print output
         
