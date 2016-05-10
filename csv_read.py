dfe739cfd7b883b59be9513e3502240aeecffb2c
adina-voicu <s.adynna@yahoo.com>
2016-05-10 12:33:14 +0300

    Update csv_read.py

--- csv_read.py
+++ csv_read.py
@@ -4,6 +4,9 @@ from nltk.stem import WordNetLemmatizer
 from nltk import pos_tag
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
+import pymongo
+from pymongo import MongoClient
+
 wnl = WordNetLemmatizer()
 #obtain a clean sentence
 def clean(str):
@@ -15,15 +18,30 @@ def clean_sentence(s):
         for items in s:
             lst_cleaned.append(clean(items))
         return lst_cleaned
+
+def pos(tag):
+    if tag.startswith('JJ'):
+        return 'a'
+    elif tag.startswith('VB'):
+        return 'v'
+    elif tag.startswith('NN'):
+        return 'n'
+    elif tag.startswith('RB'):
+        return 'r'
+    else:
+        return ''
+
 # lemmatize text         
-def lemma(txt):
+def lemma(sentences):
     a=[]
-    for i in txt:
-        x = ' '.join([wnl.lemmatize(i,j[0].lower()) if j[0].lower() in ['a','n','v'] else wnl.lemmatize(i) for i,j in pos_tag(word_tokenize(i))])
+    for sentence in sentences:
+        x = ' '.join([wnl.lemmatize(word, pos(tag)) for word, tag in pos_tag(word_tokenize(sentence)) if pos(tag) in ['n', 'v', 'a', 'r'] ])
+
         a.append(x)
     return a
 #remove stopwords
 def stop(txt):
+    stopwords = nltk.corpus.stopwords.words('english')
     x=[]
     for w in txt.split():
         if w not in stopwords:
@@ -31,6 +49,16 @@ def stop(txt):
         else:
             continue
     return ' '.join(x)
+#insert docs in mongodb
+def insert(docs):
+    con = pymongo.MongoClient()
+    coll = con.test.docs
+    #docs = [{"_id" : 2, "foio" : "HELLO world"}, {"_id" : 2, "Blah ah" : "Bloh"}]
+    for doc in docs:
+        coll.save(doc)
+   
+#   for d in docs:
+#        db.test_collection.insert(d)
 
 with open('datec.csv') as csvfile:
     reader = csv.DictReader(csvfile)
@@ -40,7 +68,7 @@ with open('datec.csv') as csvfile:
         row={}
         for field in header:
             if field == 'abstract':
-                row['abstract'] = stop(' '.join(lemma(clean_sentence(nltk.sent_tokenize(line['abstract'])))))
+                row['abstract'] = insert(stop(' '.join(lemma(clean_sentence(nltk.sent_tokenize(line['abstract']))))))
             else:    
                 row[field]=line[field]
         output.append(row)
