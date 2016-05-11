ca9fccb599f8e0d3a8f995ef957922a19e9ef2dc
adina-voicu <s.adynna@yahoo.com>
2016-05-11 13:57:41 +0300

    Update LDA.py

--- LDA.py
+++ LDA.py
@@ -1,31 +1,35 @@
-import numpy as np
+# de pe pagina 1 https://radimrehurek.com/gensim/tut1.html
+
 from gensim import corpora, models, similarities
-import gensim
-import lda
-import lda.datasets
 import pymongo
 from pymongo import MongoClient
+from random import randint
 
 con = pymongo.MongoClient()
 collection = con.test.docs
+#docs = [{"_id" : 2, "foo" : "HELLO"}, {"_id" : 2, "Blah" : "Bloh"}]
 docs = list(collection.find())
 #print docs
-documents = []    
+documents = []
 for doc in docs:
     text = doc['abstract']
     documents.append(text)
 #print(documents)
+themes = []
+for doc in docs:
+    text = doc['theme']
+    themes.append(text)
 
 # vectorizarea documentelor, o lista de liste cu termeni:
 texts = [[word for word in document.lower().split()] for document in documents]
 #print texts
+
 #dictionar id 2 word
 dictionary = corpora.Dictionary(texts)
 #for key in dictionary:
 #    print key, dictionary[key]
 
 # lista de liste cu termeni si frecventa (co-occurrence) 
-# in literatura bag-of-words integer counts
 corpus = [dictionary.doc2bow(text) for text in texts]
 #print corpus
 
@@ -33,12 +37,43 @@ corpus = [dictionary.doc2bow(text) for text in texts]
 tfidf = models.TfidfModel(corpus)
 
 # vectorizare TF*IDF
-# Aceasta este matricea despre care iti ziceam in mail
 corpus_tfidf = tfidf[corpus]
 
+#for doc in corpus_tfidf:
+#    print doc
 
 # generate LDA model
-ldamodel = gensim.models.ldamodel.LdaModel(corpus, num_topics=5, id2word = dictionary, passes=20)
+lda = gensim.models.ldamodel.LdaModel(corpus_tfidf, num_topics=5, id2word = dictionary, passes=20)
+
 print(ldamodel.print_topics(num_topics=5, num_words=10))
 
 
+# pentru a vedea probabilitatea cu care apartine un documnet la un topic
+corpus_lda = lda[corpus_tfidf]
+top = []
+for l in corpus_lda:
+#    print l
+    if not l:
+        topic = randint(0,4)
+    else:
+        maximum = l[0][1]
+    for elem in l:
+        if maximum< elem[1]:
+            maximum = elem[1]
+            topic = elem[0]
+    top.append(topic)
+print top
+print themes
+thtop = zip(themes,top)
+print z
+
+clase = {
+    'medical' : {0:0, 1:0, 2:0, 3:0, 4:0},
+    'database' : {0:0, 1:0, 2:0, 3:0, 4:0},
+    'theory' : {0:0, 1:0, 2:0, 3:0, 4:0},
+    'datamining' : {0:0, 1:0, 2:0, 3:0, 4:0},
+    'visu' : {0:0, 1:0, 2:0, 3:0, 4:0}
+}
+for i in thtop:
+    clase[i[0]][i[1]] +=1
+print clase
