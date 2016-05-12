fbf38c11c92ce9bc7fafb48d4e344073655e5bb2
adina-voicu <s.adynna@yahoo.com>
2016-05-13 02:21:10 +0300

    Update LDA.py

--- LDA.py
+++ LDA.py
@@ -6,7 +6,7 @@ import pymongo
 from pymongo import MongoClient
 
 con = pymongo.MongoClient()
-collection = con.test.docs
+collection = con.test.documente
 docs = list(collection.find())
 #print docs
 documents = []
@@ -20,6 +20,7 @@ for doc in docs:
     themes.append(text)
 
 # vectorizarea documentelor, o lista de liste cu termeni:
+# aceast lista trebuie sa contina doar lemme. 
 texts = [[word for word in document.lower().split()] for document in documents]
 #print texts
 
@@ -38,14 +39,14 @@ tfidf = models.TfidfModel(corpus)
 
 # vectorizare TF*IDF
 corpus_tfidf = tfidf[corpus]
+
 #for doc in corpus_tfidf:
 #    print doc
 
-# generate LDA model
+# generare LDA model
 lda = models.ldamodel.LdaModel(corpus_tfidf, num_topics=5, id2word = dictionary, passes=350)
 
 print (lda.print_topics(num_topics=5, num_words=10))
-
 
 # pentru a vedea probabilitatea cu care apartine un documnet la un topic
 corpus_lda = lda[corpus_tfidf]
@@ -53,19 +54,19 @@ top = []
 for l in corpus_lda:
 #    print l
     if not l:
-        topic = randint(0,4)
+        topc = randint(0,4)
     else:
         maximum = l[0][1]
     for elem in l:
         if maximum< elem[1]:
             maximum = elem[1]
-            topic = elem[0]
-    top.append(topic)
-print top
-print themes
+            topc = elem[0]
+    top.append(topc)
+#print top
+#print themes
 thtop = zip(themes,top)
-print z
-
+#print thtop
+# Generare tabel de contingență
 clase = {
     'medical' : {0:0, 1:0, 2:0, 3:0, 4:0},
     'database' : {0:0, 1:0, 2:0, 3:0, 4:0},
